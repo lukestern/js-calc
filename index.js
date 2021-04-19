edb4e0b171d9a6125ffddd849b5631f14ea59cc4
Luke Stern <luke.stern@softwire.com>
2021-04-19 13:29:33 +0100

    Functions refactor

--- index.js
+++ index.js
@@ -1,33 +1,89 @@
 const readline = require("readline-sync");
 
 
-console.log('Welcome to the Calculator!');
-
-console.log('Please enter an operator (+, -, * or /):');
-const operation = readline.prompt();
-
-console.log(`How many numbers do you want to ${operation}?`)
-const repetitions = Number(readline.prompt());
-
-const inputs = [];
-for (var i = 0; i < repetitions; i++) {
-    console.log(`Please enter number ${i+1}:`);
-    inputs[i] = Number(readline.prompt());
+function printWelcomeMessage(numOfOperations) {
+    console.log('\nWelcome to the Calculator!');
 }
 
-switch (operation)  {
-    case '+':
-        var result = inputs.reduce((a, b) => a + b);
-        break;
-    case '-':
-        var result = inputs.reduce((a, b) => a - b);
-        break;
-    case '*':
-        var result = inputs.reduce((a, b) => a * b);
-        break;
-    case '/':
-        var result = inputs.reduce((a, b) => a / b);
-        break;
+function getInputs(){
+    inputs = [];
+    inputs[0] = getOperatorInput()
+    if (inputs[0]) {
+        return getNumericalInputs(inputs)    
+    } else {
+        return null
+    }
 }
 
-console.log('Reuslt: ' + result)
+function getOperatorInput() {
+    console.log('\nPlease enter an operator (+, -, * or /):');
+    operator = readline.prompt();
+    if (operator === '+' || operator === '-' || operator === '*' || operator === '/') {
+        return operator
+    } else {
+        console.log('An invalid operator was given.')
+        return null
+    }
+}
+
+function getNumericalInputs(inputs) {
+    console.log(`How many numbers do you want to ${inputs[0]}?`);
+    const repetitions = Number(readline.prompt());
+    if (valueIsNumber(repetitions)) {
+        for (var i = 1; i <= repetitions; i++) {
+            console.log(`Please enter number ${i}:`);
+            let value = Number(readline.prompt());
+            if (valueIsNumber(value)) {
+                inputs.push(value);
+            }
+        }
+    } else {
+        console.log('An invalid number of repetitions was given.');
+        return null;
+    }
+    return inputs
+}
+
+function valueIsNumber(value) {
+        if (isNaN(value)) {
+            console.log('A non-numerical value was given.')
+            return false
+        } else {
+            return true;
+        }
+}
+
+function calculate(inputArray) {
+    switch (inputArray[0])  {
+        case '+':
+            var result = inputArray.slice(1).reduce((a, b) => a + b);
+            break;
+        case '-':
+            var result = inputArray.slice(1).reduce((a, b) => a - b);
+            break;
+        case '*':
+            var result = inputArray.slice(1).reduce((a, b) => a * b);
+            break;
+        case '/':
+            var result = inputArray.slice(1).reduce((a, b) => a / b);
+            break;
+    }
+    return result
+}
+
+function doOneCalculation() {
+    inputArray = getInputs()
+    if (inputArray) { 
+        result = calculate(inputArray)
+        console.log('Reuslt: ' + result);
+    }
+}
+
+var numOfOperations = 0;
+while (numOfOperations >= 0) {
+    if (numOfOperations === 0) {
+        printWelcomeMessage()
+    }
+    doOneCalculation()
+    numOfOperations += 1
+}
