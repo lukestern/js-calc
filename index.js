d73f943f78a254fee991c4fe785ebe44308271d7
Luke Stern <luke.stern@softwire.com>
2021-04-19 12:25:53 +0100

    Loops and Arrays

--- index.js
+++ index.js
@@ -6,24 +6,27 @@ console.log('Welcome to the Calculator!');
 console.log('Please enter an operator (+, -, * or /):');
 const operation = readline.prompt();
 
-console.log('Please enter a number:');
-const inputOne = Number(readline.prompt());
+console.log(`How many numbers do you want to ${operation}?`)
+const repetitions = Number(readline.prompt());
 
-console.log('Please enter another number:');
-const inputTwo = Number(readline.prompt());
+const inputs = [];
+for (var i = 0; i < repetitions; i++) {
+    console.log(`Please enter number ${i+1}:`);
+    inputs[i] = Number(readline.prompt());
+}
 
 switch (operation)  {
     case '+':
-        var result = inputOne + inputTwo;
+        var result = inputs.reduce((a, b) => a + b);
         break;
     case '-':
-        var result = inputOne - inputTwo;
+        var result = inputs.reduce((a, b) => a - b);
         break;
     case '*':
-        var result = inputOne * inputTwo;
+        var result = inputs.reduce((a, b) => a * b);
         break;
     case '/':
-        var result = inputOne / inputTwo;
+        var result = inputs.reduce((a, b) => a / b);
         break;
 }
 
